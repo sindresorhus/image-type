051158353e17a594e294b53e93bc6ef1189f58c3
Sindre Sorhus <sindresorhus@gmail.com>
2014-05-01 00:27:02 +0200

    0.1.4

--- browser.js
+++ browser.js
@@ -65,14 +65,13 @@ module.exports = function (buf) {
 },{}],4:[function(_dereq_,module,exports){
 'use strict';
 module.exports = function (buf) {
-	if (!buf || buf.length < 4) {
+	if (!buf || buf.length < 3) {
 		return false;
 	}
 
 	return buf[0] === 255 &&
 		buf[1] === 216 &&
-		buf[2] === 255 &&
-		(buf[3] === 224 || buf[3] === 225);
+		buf[2] === 255;
 };
 
 },{}],5:[function(_dereq_,module,exports){
