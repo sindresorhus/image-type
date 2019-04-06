25fd70fc5bba1833c542442a34af60c76b7e87fa
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 02:15:35 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,80 @@
 /// <reference types="node"/>
 
-export type ImageType =
-	| 'jpg'
-	| 'png'
-	| 'gif'
-	| 'webp'
-	| 'flif'
-	| 'cr2'
-	| 'tif'
-	| 'bmp'
-	| 'jxr'
-	| 'psd'
-	| 'ico'
-	| 'bpg'
-	| 'jp2'
-	| 'jpm'
-	| 'jpx'
-	| 'heic'
-	| 'cur'
-	| 'dcm';
+declare namespace imageType {
+	type ImageType =
+		| 'jpg'
+		| 'png'
+		| 'gif'
+		| 'webp'
+		| 'flif'
+		| 'cr2'
+		| 'tif'
+		| 'bmp'
+		| 'jxr'
+		| 'psd'
+		| 'ico'
+		| 'bpg'
+		| 'jp2'
+		| 'jpm'
+		| 'jpx'
+		| 'heic'
+		| 'cur'
+		| 'dcm';
 
-export interface ImageTypeResult {
-	/**
-	 * One of the supported [file types](https://github.com/sindresorhus/image-type#supported-file-types).
-	 */
-	ext: ImageType;
+	interface ImageTypeResult {
+		/**
+		One of the supported [file types](https://github.com/sindresorhus/image-type#supported-file-types).
+		*/
+		ext: ImageType;
 
-	/**
-	 * The detected [MIME type](https://en.wikipedia.org/wiki/Internet_media_type).
-	 */
-	mime: string;
+		/**
+		The detected [MIME type](https://en.wikipedia.org/wiki/Internet_media_type).
+		*/
+		mime: string;
+	}
 }
 
-/**
- * Detect the image type of a `Buffer`/`Uint8Array`.
- *
- * @param input - Input to examine to determine the file type. It only needs the first `.minimumBytes` bytes.
- */
 declare const imageType: {
-	(input: Buffer | Uint8Array): ImageTypeResult | null;
+	/**
+	Detect the image type of a `Buffer`/`Uint8Array`.
+
+	@param input - Input to examine to determine the file type. It only needs the first `.minimumBytes` bytes.
+
+	@example
+	```
+	import readChunk = require('read-chunk');
+	import imageType = require('image-type');
+
+	const buffer = readChunk.sync('unicorn.png', 0, 12);
+
+	imageType(buffer);
+	//=> {ext: 'png', mime: 'image/png'}
+
+
+	// Or from a remote location:
+	import * as http from 'http';
+
+	const url = 'https://assets-cdn.github.com/images/spinners/octocat-spinner-32.gif';
+
+	http.get(url, response => {
+		response.on('readable', () => {
+			const chunk = response.read(imageType.minimumBytes);
+			response.destroy();
+			console.log(imageType(chunk));
+			//=> {ext: 'gif', mime: 'image/gif'}
+		});
+	});
+	```
+	*/
+	(input: Buffer | Uint8Array): imageType.ImageTypeResult | null;
 
 	/**
-	 * The minimum amount of bytes needed to detect a file type. Currently, it's 4100 bytes, but it can change, so don't hard-code it.
-	 */
+	The minimum amount of bytes needed to detect a file type. Currently, it's 4100 bytes, but it can change, so don't hard-code it.
+	*/
 	readonly minimumBytes: number;
+
+	// TODO: Remove this for the next major release
+	default: typeof imageType;
 };
 
-export default imageType;
+export = imageType;
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,6 @@
-import {expectType} from 'tsd-check';
-import imageType, {ImageTypeResult, ImageType} from '.';
+import {expectType} from 'tsd';
+import imageType = require('.');
+import {ImageTypeResult, ImageType} from '.';
 
 imageType(new Buffer([0xff, 0xd8, 0xff]));
 imageType(new Uint8Array([0xff, 0xd8, 0xff]));
